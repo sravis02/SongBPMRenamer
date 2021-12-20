8dc5062634129c6913ae2e53631006d3f0499314
sravis02 <Iheyk2002@gmail.com>
2021-12-20 22:36:02 +0100

    Added Requestheaders

---
+++ headers.py
@@ -0,0 +1,17 @@
+headers = {
+    'authority': 'www.beatport.com',
+    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
+    'sec-ch-ua-mobile': '?0',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
+    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'accept': 'text/html, */*; q=0.01',
+    'x-requested-with': 'XMLHttpRequest',
+    'x-pjax': 'true',
+    'x-pjax-container': '#pjax-inner-wrapper',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-dest': 'empty',
+    'referer': 'https://www.beatport.com/track/ommp/663810',
+    'accept-language': 'de-DE,de;q=0.9',
+}
