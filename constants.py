2b2452aa2b7c8c1b1089f1e43762a48e6f5d4357
sravis02 <Iheyk2002@gmail.com>
2021-12-21 18:43:50 +0100

    Created constants

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-headers = {
+HEADERS = {
     'authority': 'www.beatport.com',
     'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
     'sec-ch-ua-mobile': '?0',
@@ -15,3 +15,6 @@ headers = {
     'referer': 'https://www.beatport.com/track/ommp/663810',
     'accept-language': 'de-DE,de;q=0.9',
 }
+
+# mp4 can also be an audio file :D
+AUDIO_FILE_TYPES = ["m4a", "flac", "mp3", "mp4", "wav", "wma", "aac"]
